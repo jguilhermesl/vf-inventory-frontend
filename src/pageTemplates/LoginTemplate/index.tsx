ee57ccb3a1003a5231b0330f8d6f5cf7974afa8f
CaioAlmeida09 <Caio09092001@gmail.com>
2024-01-25 22:52:08 -0300

    Fazendo validação dos campos de e-mail e senha utilizando formik e yup

--- src/pageTemplates/LoginTemplate/index.tsx
+++ src/pageTemplates/LoginTemplate/index.tsx
@@ -1,29 +1,78 @@
-import { Button } from '@/components/Button';
-import { Input } from '@/components/Input';
-import { Envelope, EyeClosed, Eye } from 'phosphor-react';
-import { useState } from 'react';
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Envelope, EyeClosed, Eye } from "phosphor-react";
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Digite um e-mail válido")
+    .required("Campo obrigatório"),
+  password: Yup.string().required("Campo obrigatório"),
+});
 
 export const LoginTemplate = () => {
-  const [inputEmail, setInputEmail] = useState('');
-  const [inputPassword, setInputPassword] = useState('');
+  // const [inputEmail, setInputEmail] = useState("");
+  // const [inputPassword, setInputPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const handleEmailAndPassword = (values) => {
+    console.log("minha senha:", values.password);
+    console.log("meu email:", values.email);
+  };
   return (
-    <div className=" w-full h-screen flex flex-col justify-center m-auto md:max-w-xl w-sm p-8 ">
-      <h1 className="text-2xl font-bold mb-10 text-center">Login</h1>
-      <Input
-        label="Email"
-        onChange={(e) => setInputEmail(e.target.value)}
-        iconRight={<Envelope size={16} />}
-      />
-      <Input
-        label="Senha"
-        onChange={(e) => setInputPassword(e.target.value)}
-        type={showPassword ? 'text' : 'password'}
-        iconRight={showPassword ? <Eye size={16} /> : <EyeClosed size={16} />}
-        onClickIconRight={() => setShowPassword(!showPassword)}
-      />
-      <Button>Entrar</Button>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleEmailAndPassword}
+    >
+      <Form className="w-full h-screen flex flex-col justify-center m-auto md:max-w-xl w-sm p-8">
+        <h1 className="text-2xl font-bold mb-10 text-center">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <Field name="email">
+            {({ field, meta }) => (
+              <>
+                <Input
+                  type="text"
+                  id="email"
+                  {...field}
+                  iconRight={<Envelope size={16} />}
+                />
+                {meta.touched && meta.error && (
+                  <div className=" text-red-800">{meta.error}</div>
+                )}
+              </>
+            )}
+          </Field>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Senha</label>
+          <Field name="password">
+            {({ field, meta }) => (
+              <>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  {...field}
+                  iconRight={
+                    showPassword ? <Eye size={16} /> : <EyeClosed size={16} />
+                  }
+                  onClickIconRight={() => setShowPassword(!showPassword)}
+                />
+                {meta.touched && meta.error && (
+                  <div className=" text-red-800">{meta.error}</div>
+                )}
+              </>
+            )}
+          </Field>
+        </div>
+        <Button type="submit">Entrar</Button>
+      </Form>
+    </Formik>
   );
 };
