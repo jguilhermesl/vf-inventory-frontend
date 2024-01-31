a2639a8283d7b73b48939900d271d815a4a54b3a
João Guilherme <joaoguilherme_pe@hotmail.com>
2024-01-31 15:16:30 -0300

    fixc

---
+++ src/pageTemplates/InventoryTemplate/index.tsx
@@ -0,0 +1,9 @@
+import { LayoutWithSidebar } from '@/components/layouts/LayoutWithSidebar';
+
+export const InventoryTemplate = () => {
+  return (
+    <LayoutWithSidebar>
+      <h1>Teste</h1>
+    </LayoutWithSidebar>
+  );
+};
