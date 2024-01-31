edf63212acc8b3cd0ab2a8c913c712be3e933b4a
João Guilherme <88175581+jguilhermesl@users.noreply.github.com>
2024-01-31 15:14:14 -0300

    Delete src/pageTemplates/inventoryTemplate directory

--- src/pageTemplates/inventoryTemplate/index.tsx
+++
@@ -1,9 +0,0 @@
-import { LayoutWithSidebar } from '@/components/layouts/LayoutWithSidebar';
-
-export const InventoryTemplate = () => {
-  return (
-    <LayoutWithSidebar>
-      <h1>Layout</h1>
-    </LayoutWithSidebar>
-  );
-};
