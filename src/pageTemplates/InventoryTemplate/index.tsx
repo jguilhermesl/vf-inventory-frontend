407d3128960db7606a97f10af63a9a34687db20f
João Guilherme <joaoguilherme_pe@hotmail.com>
2024-02-05 20:37:15 -0300

    Fix: labels no estoque

--- src/pageTemplates/InventoryTemplate/index.tsx
+++ src/pageTemplates/InventoryTemplate/index.tsx
@@ -29,14 +29,14 @@ export const InventoryTemplate = () => {
           <div className="mb-6 flex items-center justify-center text-center lg:text-start lg:justify-between flex-col lg:flex-row">
             <div className="flex flex-col mb-4 lg:mb-0">
               <Heading>Estoque</Heading>
-              <Paragraph>Gerencie seu Estoque</Paragraph>
+              <Paragraph>Gerencie seu estoque</Paragraph>
             </div>
             <Button
-              className="!w-[250px]"
+              className="!w-[255px]"
               leftIcon={<PlusCircle color="#FFF" size={16} />}
               onClick={() => setmodalAddInventoryIsOpen(true)}
             >
-              Adicionar Estoque
+              Adicionar estoque
             </Button>
           </div>
           <Table content={inventory} handleEditItem={handleEditInventory} />
